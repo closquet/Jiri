4fbda6d9eb045d3c6b332e8b4bb4bd43b26d31f1
Eric Closquet <closquet.eric@live.be>
2018-01-07 16:23:55 +0100

    add softdelete to user model

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace Jiri;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
@@ -31,7 +32,7 @@ use Laravel\Passport\HasApiTokens;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -39,7 +40,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+	    'name', 'email', 'password', 'is_admin', 'company', 'available',
     ];
 
     /**
@@ -50,4 +51,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+	
+	/**
+	 * The attributes that should be mutated to dates.
+	 *
+	 * @var array
+	 */
+	protected $dates = ['deleted_at'];
 }
